b15d4245f3592a65e492088a9e9b04553bca22e6
Kirill Cherkashin <kirill@kcherkashin.org>
2015-03-06 17:37:18 -0500

    Get the slide number from the url on start

--- src/modules/History.jsx
+++ src/modules/History.jsx
@@ -2,6 +2,7 @@ class History {
   constructor(store, slideIndexCallback) {
     this.store = store;
     this.slideIndexCallback = slideIndexCallback;
+    this.updateSlideIndex();
   }
 
   updateSlideIndex() {
